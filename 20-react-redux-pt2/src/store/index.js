9a7589b7bfb832be08767387a7d3823b0f3ccb9b
Brad <ibanezbradley@gmail.com>
2026-07-25 22:49:18 +0800

    React Redux: Making handlers with dispatchers flexible
    We can add in extra properties inside the object dispatch to pass in values.
    
    Then we can access and utilize this property within reduction that is based on the value passed and not something that is hard coded

--- 20-react-redux-pt2/src/store/index.js
+++ 20-react-redux-pt2/src/store/index.js
@@ -4,6 +4,10 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return { ...state, counter: state.counter + 1 };
   }
+
+  if (action.type === "increase") {
+    return { ...state, counter: state.counter + action.amount };
+  }
   if (action.type === "decrement") {
     return { ...state, counter: state.counter - 1 };
   }
